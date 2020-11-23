95546c5e74a054e9b5d001674b9cbe47cb8e7998
Rodolfo Cortese <23123652+rcortese@users.noreply.github.com>
2020-11-22 21:25:55 -0300

    tagging example

--- main.go
+++ main.go
@@ -6,4 +6,29 @@ func main() {
 
 	fmt.Println("Hello World!")
 
+here:
+	for i := 0; i < 2; i++ {
+		for j := i + 1; j < 3; j++ {
+			if i == 0 {
+				continue here
+			}
+			fmt.Println(j)
+			if j == 2 {
+				break
+			}
+		}
+	}
+
+there:
+	for i := 0; i < 2; i++ {
+		for j := i + 1; j < 3; j++ {
+			if j == 1 {
+				continue
+			}
+			fmt.Println(j)
+			if j == 2 {
+				break there
+			}
+		}
+	}
 }
